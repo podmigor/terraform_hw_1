34499bb29934bd767e1c5d137184ff17e2075e53
Igor Podmogylnyi <podmigor@gmail.com>
2022-05-21 00:42:05 +0000

    finished

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "google_storage_bucket" "default" {
+  name          = "hillel_18_podmogylnyi"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_compute_instance" "default" {
+  name         = "compute-instance"
+  machine_type = "f1-micro"
+
+#  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-9-stretch-v20220519"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "apt update && apt install nginx -y"
+}
+
+output "exteral_ip" {
+  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+}
+
+output "bucket_url" {
+  value = google_storage_bucket.default.self_link
+}
---
+++ provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      version = "4.15.0"
+      source  = "hashicorp/google"
+    }
+
+  }
+}
+
+
+
+provider "google" {
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials
+}
+
+
+variable "project" {
+ type = string
+}
+
+variable "region" {
+ type = string
+}
+
+variable "zone" {
+ type = string
+}
+
+variable "credentials" {
+ type = string
+}
